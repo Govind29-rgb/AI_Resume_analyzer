bc683751da688f405355495bef4ef16e909ddb92
Govind29-rgb <govoffi29@gmail.com>
2025-07-24 10:24:54 +0530

    index.d.ts_defined

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,29 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+  {/* //navbar component imported here */}
+
+  <Navbar/>
+
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume</h1>
+        <h2>Review your submissions and check AI_powered feedback</h2>
+      </div>
+
+      {/* here we import data in array format like title,etc info ..instead of doing here we write in index.ts in constants file ..it would form a db */}
+
+    </section>
+  </main>
 }
